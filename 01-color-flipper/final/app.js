4a9af25c8e94a2bda0be5c4ecaa90f1ba339d694
b2academonicholas <85600561+b2academonicholas@users.noreply.github.com>
2022-02-15 15:19:41 +0400

    first commit

--- 01-color-flipper/final/app.js
+++ 01-color-flipper/final/app.js
@@ -1,15 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
-  // console.log(randomNumber);
+const btn = document.getElementById('btn');
+const color = document.querySelector ('.color');
 
+btn.addEventListener('click' , function() {
+  // get random number btwn 0-3
+  const randomNumber = 2;
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
+  return Math.floor (random() * colors.length);
 }
